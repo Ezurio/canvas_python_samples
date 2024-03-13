e77b7ccc560e19e3ab5329094ae61449c56ba2ce
Ryan Erickson <ryan.erickson@lairdconnect.com>
2024-03-13 16:42:53 -0500

    sample: mqtts: add topic subscription
    Add support for subscribing to a topic to receive messages from the server.
    
    Add ping timer to keep the connection alive.
    
    PROD-8129
    
    Signed-off-by: Ryan Erickson <ryan.erickson@lairdconnect.com>

--- networking/mqtt/main_mqtts.py
+++ networking/mqtt/main_mqtts.py
@@ -1,6 +1,8 @@
-import time
 from net_helper import NetHelper
 from mqtt import MQTTClient
+import binascii
+import machine
+import canvas
 
 ##############################################################################
 # Configuration
@@ -9,7 +11,7 @@ from mqtt import MQTTClient
 # For simplicity, the certificates/keys are stored as strings in this example.
 # In a real application, you would store them in a more secure location.
 
-client_id = "my-device"
+client_id = "my-device_" + binascii.hexlify(machine.unique_id()).decode()
 hostname = "mqtt.example.com"
 port = 8883
 client_cert = """-----BEGIN CERTIFICATE-----
@@ -24,15 +26,23 @@ ca_cert = """-----BEGIN CERTIFICATE-----
 ...
 -----END CERTIFICATE-----
 """
-topic = "my-topic"
-data = "Hello, MQTT!"
+
+# publish topic
+topic_pub = "device/" + client_id + "/data"
+data_pub = '{"messge":"hello!"}'
+
+# subscribe topic
+topic_sub = "device/" + client_id + "/control"
+
+MQTT_KEEP_ALIVE_SECONDS = 300
+MSG_WAIT_TIMEOUT_MS = 30000
 
 #
 ##############################################################################
 
-# Wait for network to come up
-print("Waiting for network")
+print("Running MQTTS example")
 net = NetHelper(None)
+print("Waiting for network...")
 net.wait_for_ready()
 
 # Connect the MQTT client
@@ -40,19 +50,43 @@ mqtt = MQTTClient(
     client_id,
     hostname,
     port=port,
-    keepalive=60,
+    keepalive=MQTT_KEEP_ALIVE_SECONDS,
     ssl=True,
-    ssl_params = {
+    ssl_params={
         "cert": client_cert,
         "key": client_key,
         "cadata": ca_cert,
         "server_hostname": hostname
     })
+
+
+def subscribe_cb(topic, msg):
+    print("MQTT subscribe msg", msg)
+
+
+def mqtt_ping_timer_cb(event):
+    print("MQTT ping")
+    mqtt.ping()
+
+
+mqtt.set_callback(subscribe_cb)
+
+print("Connecting to MQTT as endpoint: {}".format(client_id))
 mqtt.connect()
 
-# Publish a message
-print("Publishing message")
-mqtt.publish(topic, data)
+print("Subscribing to topic: {}".format(topic_sub))
+mqtt.subscribe(topic_sub)
 
-# Disconnect the MQTT client
-mqtt.disconnect()
+print("Publishing message to topic: {} with data: {}".format(topic_pub, data_pub))
+mqtt.publish(topic_pub, data_pub)
+
+mqtt_ping_timer = canvas.Timer(
+    MQTT_KEEP_ALIVE_SECONDS * 1000, True, mqtt_ping_timer_cb, None)
+mqtt_ping_timer.start()
+
+while True:
+    print("Waiting for MQTT message...")
+    try:
+        mqtt.wait_msg(MSG_WAIT_TIMEOUT_MS)
+    except Exception as e:
+        print(e)
