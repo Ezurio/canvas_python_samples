5f0adbc816a7fa6c1d37f98266cd3c00e1ca92cc
scottlederer-lci <scott.lederer@lairdconnect.com>
2024-06-28 10:09:26 -0500

    Improved hello world sample based on FAE feedback. Added reload.py to reload/run scripts from the REPL. This is included in this commit as it is related to the "import" approach to run scripts as described in the comments of the new hello_world.py sample.
    PROD-12790

--- basic/hello_world.py
+++ basic/hello_world.py
@@ -1,5 +1,21 @@
-str = "hello world :"
-val = 0
-while val < 1000:
-    print(str,  val, "\r\n")
-    val += 1
+#
+# hello_world.py
+#
+# This sample prints a message to the console in a loop.
+#
+# To use this sample, connect to your device using Xbit tools for VS Code
+# (https://marketplace.visualstudio.com/items?itemName=rfp-canvas.xbit-vsc)
+# and save the content of this file as 'hello_world.py' to the device.
+# Click the device name in the USB DEVICES panel to open a TERMINAL providing
+# access to the REPL serial port of your device. Press 'Enter' in the TERMINAL
+# tab and ensure you see the '>>>' prompt. You can run the 'hello_world.py'
+# script by typing 'import hello_world'. The hello message will print 20 times.
+#
+# NOTE: Because of how the Python import statement works, you will need to
+# reset the device if you would like to run the script again.
+#
+str = "Hello, world!"
+counter = 0
+while counter < 20:
+    print(str,  counter)
+    counter += 1
