aeb4269bb37167a85c95f4c211ea7c9404c9e09f
Electronick <nicolas.velasquez531@gmail.com>
2025-06-24 22:33:12 -0700

    changed order of ssl.wrap_socket. Had to init it first before calling it

--- starter_projects/lib/networking/mqtt_client/mqtt_client.py
+++ starter_projects/lib/networking/mqtt_client/mqtt_client.py
@@ -75,11 +75,11 @@ class MQTTClient:
         self.sock = socket.socket()
         self.sock.setblocking(True)
         addr = socket.getaddrinfo(self.server, self.port)[0][-1]
+        self.sock.connect(addr)
         if self.ssl:
             import ssl
 
             self.sock = ssl.wrap_socket(self.sock, **self.ssl_params)
-        self.sock.connect(addr)
         premsg = bytearray(b"\x10\0\0\0\0\0")
         msg = bytearray(b"\x04MQTT\x04\x02\0\0")
 
