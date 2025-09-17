c3ca8c72233ca9910f251297f6e6fffe67f6e05f
Ryan Erickson <ryan.erickson@ezurio.com>
2025-09-17 15:57:59 -0500

    peripherals: CAN: add sender/replier samples
    Demonstrate sending and receiving messages between two boards.
    Support putting the CAN device to sleep.
    
    PROD-29636
    
    Signed-off-by: Ryan Erickson <ryan.erickson@ezurio.com>

---
+++ snippets/peripherals/can/replier/main.py
@@ -0,0 +1,93 @@
+"""Listens for CAN frames and replies to each frame received by incrementing the CAN ID by 1.
+Ctrl-C to exit the monitor_and_reply() loop.
+Functions can then be called from the REPL.
+
+Use this sample with another board running ../sender/main.py
+"""
+import os
+import time
+from machine import CAN
+from machine import Pin
+
+
+class Replier:
+    def __init__(self):
+        self.REPLY_INTERVAL_MS = 100  # Interval to send replies
+        self.REPLY_TIMEOUT_MS = 500  # Timeout for sending a reply
+
+        self.can_queue = []
+
+        if "bl54l" in os.uname().machine:
+            # Set WKP low to avoid excessive current draw on CAN FD 6 click
+            wkp = Pin("P1_14", Pin.OUT, 0)
+            wkp.off()
+
+        self.can = CAN(CAN.MODE_NORMAL, False, self.can_rx_callback)
+        self.sleeping = False
+        self.filters = []
+
+    def can_rx_callback(self, frame: tuple):
+        try:
+            self.can_queue.append(frame)
+        except Exception as e:
+            print("Error appending frame to queue:", e)
+
+    def can_sleep(self):
+        for f in self.filters:
+            self.can.remove_filter(f)
+        self.filters.clear()
+        self.can.suspend()
+        self.sleeping = True
+        print("CAN in sleep mode")
+
+    def can_resume(self):
+        try:
+            self.can.resume()
+            self.can.stop()
+        except:
+            pass
+        # Accept all frames
+        self.filters.append(self.can.add_filter(0, 0, 0))
+        self.filters.append(self.can.add_filter(0, 0, CAN.FILTER_IDE))
+        self.can.set_bitrate(250000)
+        self.can.start()
+
+    def restart_can(self):
+        if self.sleeping:
+            return
+        self.can.stop()
+        self.can.start()
+
+    def clear_stats(self):
+        self.can.get_stats(True)
+
+    def stats(self, print_stats=True):
+        stats = self.can.get_stats()
+        if print_stats:
+            print(stats)
+        return stats
+
+    def send_reply(self):
+        if (len(self.can_queue) == 0):
+            return
+        print("Can queue length:", len(self.can_queue))
+        while len(self.can_queue) > 0:
+            frame = self.can_queue.pop(0)
+            can_id, rtr, data = frame
+            try:
+                # Increment the CAN ID for the reply to lower the priority and avoid collisions
+                self.can.send(can_id + 1, rtr, data, self.REPLY_TIMEOUT_MS)
+            except Exception as e:
+                print("Error sending:", e)
+        self.stats()
+
+    def monitor_and_reply(self):
+        self.can_resume()
+        while True:
+            self.send_reply()
+            time.sleep_ms(self.REPLY_INTERVAL_MS)
+
+
+print("CAN bus started. Listening for frames...")
+r = Replier()
+r.monitor_and_reply()
---
+++ snippets/peripherals/can/sender/main.py
@@ -0,0 +1,89 @@
+"""This example will send 1000 messages on boot.
+After sending the messages it will put the CAN peripheral to sleep.
+
+send_messages() can be called again to send another 1000 messages.
+"""
+import os
+import time
+from machine import CAN
+from machine import Pin
+
+
+class Sender:
+    def __init__(self):
+        self.SEND_TIMEOUT_MS = 500  # Timeout in milliseconds
+        self.SEND_ID = 89218231  # ID for the CAN message
+        self.SEND_DATA = b"12345678"  # Data to send
+        self.SEND_AMOUNT = 1000  # Number of messages to send
+        self.SLEEP_DELAY_MS = 2000  # Delay before sleeping in milliseconds
+
+        if "bl54l" in os.uname().machine:
+            # Set WKP low to avoid excessive current draw on CAN FD 6 click
+            wkp = Pin("P1_14", Pin.OUT, 0)
+            wkp.off()
+
+        self.can = CAN(CAN.MODE_NORMAL, False, self.can_rx_callback)
+        self.sleeping = False
+        self.filters = []
+
+    def can_rx_callback(self, frame: tuple):
+        pass
+
+    def can_sleep(self):
+        for f in self.filters:
+            self.can.remove_filter(f)
+        self.filters.clear()
+        self.can.suspend()
+        self.sleeping = True
+        print("CAN in sleep mode")
+
+    def can_resume(self):
+        try:
+            self.can.resume()
+            self.can.stop()
+        except:
+            pass
+        # Accept all frames
+        self.filters.append(self.can.add_filter(0, 0, 0))
+        self.filters.append(self.can.add_filter(0, 0, CAN.FILTER_IDE))
+        self.can.set_bitrate(250000)
+        self.can.start()
+
+    def restart_can(self):
+        if self.sleeping:
+            return
+        self.can.stop()
+        self.can.start()
+
+    def clear_stats(self):
+        self.can.get_stats(True)
+
+    def stats(self, print_stats=True):
+        stats = self.can.get_stats()
+        if print_stats:
+            print(stats)
+        return stats
+
+    def send_messages(self):
+        self.can_resume()
+        count = 0
+        print("Sending {} messages...".format(self.SEND_AMOUNT))
+        start_time = time.ticks_ms()
+        while count < self.SEND_AMOUNT:
+            try:
+                self.can.send(self.SEND_ID, CAN.FRAME_IDE,
+                              self.SEND_DATA, self.SEND_TIMEOUT_MS)
+            except Exception as e:
+                print("Error sending:", e)
+            count += 1
+        end_time = time.ticks_ms()
+        elapsed = time.ticks_diff(end_time, start_time)
+        print("Sent in {} ms".format(elapsed))
+        print("Wait {} ms before sleep".format(self.SLEEP_DELAY_MS))
+        time.sleep_ms(self.SLEEP_DELAY_MS)
+        self.can_sleep()
+        print("CAN stats: {}".format(self.stats(False)))
+
+
+s = Sender()
+s.send_messages()
